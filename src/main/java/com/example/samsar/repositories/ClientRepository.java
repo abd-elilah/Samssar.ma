cc731eed84615764d04194d0f075e7d2f6242f7c
abd-elilah <abdelilaboudribila0712@gmail.com>
2022-06-09 21:33:14 +0100

    "test "

--- src/main/java/com/example/samsar/repositories/ClientRepository.java
+++ src/main/java/com/example/samsar/repositories/ClientRepository.java
@@ -1,6 +1,5 @@
 package com.example.samsar.repositories;
 
-import com.example.samsar.entities.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClientRepository extends JpaRepository<Client, Long> {
